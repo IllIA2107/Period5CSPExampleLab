1ca5c7171e049abb1188b7681608c0ef8a35c4b6
IllIA2107 <illia.karmazin@vtsd.com>
2022-09-09 12:27:17 -0400

    data type update

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+// gggfj
+/*
+    hjhjhkh
+    gkkg
+    kk
+
+ */
+public class DataTypes {
+
+    int x = 4;
+    double y = 7.5;
+    boolean v = true;
+}
--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,3 +1,8 @@
 public class MyFirstProgram {
-    hhkjj
+    public static void main(String args[]){
+
+        System.out.print("Hello World!");
+
+    }
+
 }
